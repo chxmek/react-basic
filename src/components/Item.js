e8a8a6a3c60f17bc6f3e530f6ac8c1102c29120b
chxmek <Chawanwits.mek@gmail.com>
2024-08-13 23:29:45 +0700

    update code

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,7 +1,9 @@
 const Item = ()=> {
+  const name = "พักโรงแรม"
+  const amount = 5000
   return (
     <li>
-      ค่าเดินทาง <span>-200</span>
+      {name} <span>-{amount}</span>
     </li>
   );
 }
