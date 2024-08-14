51a3883cc0f269087d64f921371192c94c1ff87c
chxmek <Chawanwits.mek@gmail.com>
2024-08-14 17:57:46 +0700

    add form event

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -1,22 +1,33 @@
 import "./FormComponent.css"
 
 const FormComponent = ()=> {
+    const inputTitle = (event) => {
+        console.log(event.target.value)     // เมื่อมีการพิมพ์จะแสดงค่าผ่าน console
+    }
+    const inputAmount = (event) => {
+        console.log(event.target.value)
+    }
+    const saveItem = (event) => {
+        event.preventDefault()              // เมื่อกด submit จะไม่ทำให้ข้อมูล refresh
+        console.log("บันทึกข้อมูลเรียบร้อย")
+    }
+
     return (
-        <dive>
-            <form>
+        <div>
+            <form onSubmit={saveItem}>
                 <div className="form-control">
                     <label>ชื่อรายการ</label>
-                    <input type="text" placeholder="ระบุชื่อรายการของคุณ"></input>
+                    <input type="text" placeholder="ระบุชื่อรายการของคุณ" onChange={inputTitle}></input>
                 </div>
                 <div className="form-control">
                     <label>จำนวนเงิน</label>
-                    <input type="number" placeholder="(+ รายรับ, - รายจ่าย)"></input>
+                    <input type="number" placeholder="(+ รายรับ, - รายจ่าย)" onChange={inputAmount}></input>
                 </div>
                 <div>
                     <button className="btn" type="submit">เพิ่มข้อมูล</button>
                 </div>
             </form>
-        </dive>
+        </div>
     )
 }
 
