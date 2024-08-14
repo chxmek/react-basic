cb1ff0135311543e66c5e1ce50ff88f2ae2afa97
chxmek <Chawanwits.mek@gmail.com>
2024-08-14 14:48:34 +0700

    add uuid

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,15 +1,15 @@
 import Item from "./Item";
 import "./Transaction.css";
-import { v4 as uuidv4 } from 'uuid';
+import { v7 as uuidv7 } from 'uuid';
 
 const Transaction = () => {
   const data = [
-    { id: 1, title: "ค่ารักษาพยาบาล", amount: 2000 },
-    { id: 2, title: "ค่าน้ำมัน", amount: 5000 },
-    { id: 3, title: "ค่าเช่าบ้าน", amount: 8000 },
-    { id: 4, title: "เงินเดือน", amount: 70000 },
-    { id: 5, title: "จ่ายค่าประกัน", amount: 300 },
-    { id: 6, title: "ค่าเดินทาง", amount: 800 },
+    {title: "ค่ารักษาพยาบาล", amount: 2000 },
+    {title: "ค่าน้ำมัน", amount: 5000 },
+    {title: "ค่าเช่าบ้าน", amount: 8000 },
+    {title: "เงินเดือน", amount: 70000 },
+    {title: "จ่ายค่าประกัน", amount: 300 },
+    {title: "ค่าเดินทาง", amount: 800 },
   ];
   return (
     // <ul className="item-list">
@@ -22,7 +22,7 @@ const Transaction = () => {
     <ul className="item-list">
       {data.map((element) => {
         // return <Item title={element.title} amount={element.amount} />;   // loop ค่าแบบ array.map
-        return <Item {...element} key = {element.id}/>;                     // loop ค่าแบบ array.map, spread operator (...element) and key เพื่อให้ app รู้ว่าเป็น Item ตัวไหน
+        return <Item {...element} key = {uuidv7()}/>;                     // loop ค่าแบบ array.map, spread operator (...element) and key เพื่อให้ app รู้ว่าเป็น Item ตัวไหน
       })}
     </ul>
   );
