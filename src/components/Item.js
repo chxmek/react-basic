dd5675061debf95767cd432d25468172398532d6
chxmek <Chawanwits.mek@gmail.com>
2024-08-14 15:40:03 +0700

    add props type validation

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,10 +1,17 @@
-const Item = (props)=> {          // รับค่า props(property)
-  const {title, amount} = props   // เก็บค่าที่รับมาในตัวแปร title, amount
+import PropTypes from "prop-types";
+
+const Item = (props) => {           // รับค่า props(property)
+  const { title, amount } = props;  // เก็บค่าที่รับมาในตัวแปร title, amount
   return (
     <li>
-      {title} <span>{amount}</span>     {/* แสดงค่า property */}
+      {title} <span>{amount}</span> {/* แสดงค่า property */}
     </li>
   );
-}
+};
 
-export default Item
+Item.propTypes = {            // ให้ตรวจสอบชนิดข้อมูลว่าใช้ถูกต้องหรือไม่
+  title: PropTypes.string,
+  amount: PropTypes.number,
+};
+
+export default Item;
