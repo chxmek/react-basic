087a7c6d1d79d3f578ebd688beb8472fac391d19
chxmek <Chawanwits.mek@gmail.com>
2024-08-15 00:15:11 +0700

    add input state

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -1,21 +1,27 @@
-import { useState } from "react"
+import { useState } from "react"                // ** state คล้ายกับ props แต่ props นั้นข้อมูลจะไม่สามารถเปลี่ยนแปลงค่าได้ แต่ state เปลี่ยนแปลงค่าได้
 import "./FormComponent.css"
 
 const FormComponent = ()=> {
 
-    const [title, setTitle] = useState('')      // [ชื่อState, functionที่ใช้เปลี่ยนแปลงข้อมูลใน state] = useState(ค่าเริ่มต้นของ state)
+    const [title, setTitle] = useState('')      // * [ชื่อState, functionที่ใช้เปลี่ยนแปลงข้อมูลใน state] = useState(ค่าเริ่มต้นของ state)
     const [amount, setAmount] = useState(0)
 
     const inputTitle = (event) => {
         // console.log(event.target.value)      // เมื่อมีการพิมพ์จะแสดงค่าผ่าน console
-        setTitle(event.target.value)            // เก็บค่าลงใน state title
+        setTitle(event.target.value)            // * เก็บค่าลงใน state title
     }
     const inputAmount = (event) => {
         setAmount(event.target.value)
     }
     const saveItem = (event) => {
         event.preventDefault()                  // เมื่อกด submit จะไม่ทำให้ข้อมูล refresh
-        console.log("บันทึกข้อมูลเรียบร้อย")
+        const itemData = {                      // * สร้าง object เพื่อเก็บข้อมูลจาก form >> stateTitle, stateAmount
+            title:title,
+            amount:Number(amount)               // เปลี่ยนค่าเริ่มต้นจาก string เป็น number
+        }
+        console.log(itemData)
+        setTitle('')                            // * clear ค่าใน state
+        setAmount(0)
     }
 
     return (
@@ -23,11 +29,11 @@ const FormComponent = ()=> {
             <form onSubmit={saveItem}>              {/* ทำงานเมื่อกดปุ่ม submit */}
                 <div className="form-control">
                     <label>ชื่อรายการ</label>
-                    <input type="text" placeholder="ระบุชื่อรายการของคุณ" onChange={inputTitle}></input>
+                    <input type="text" placeholder="ระบุชื่อรายการของคุณ" onChange={inputTitle} value={title}></input>
                 </div>
                 <div className="form-control">
                     <label>จำนวนเงิน</label>
-                    <input type="number" placeholder="(+ รายรับ, - รายจ่าย)" onChange={inputAmount}></input>
+                    <input type="number" placeholder="(+ รายรับ, - รายจ่าย)" onChange={inputAmount} value={amount}></input>
                 </div>
                 <div>
                     <button className="btn" type="submit">เพิ่มข้อมูล</button>
