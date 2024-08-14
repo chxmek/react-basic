cd9ee0de4b1e6622ae142993e1189c9b2b621ddb
chxmek <Chawanwits.mek@gmail.com>
2024-08-14 17:39:38 +0700

    add style

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -1,17 +1,19 @@
+import "./FormComponent.css"
+
 const FormComponent = ()=> {
     return (
         <dive>
             <form>
                 <div className="form-control">
                     <label>ชื่อรายการ</label>
-                    <input type="text" placeholder="ระบุชื่อรานการของคุณ"></input>
+                    <input type="text" placeholder="ระบุชื่อรายการของคุณ"></input>
                 </div>
                 <div className="form-control">
                     <label>จำนวนเงิน</label>
-                    <input type="num" placeholder="ระบุจำนวนเงิน"></input>
+                    <input type="number" placeholder="(+ รายรับ, - รายจ่าย)"></input>
                 </div>
                 <div>
-                    <button type="submit">เพิ่มข้อมูล</button>
+                    <button className="btn" type="submit">เพิ่มข้อมูล</button>
                 </div>
             </form>
         </dive>
