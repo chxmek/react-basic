73a21245234791359c0f37ea59aa510c7f9d9398
chxmek <Chawanwits.mek@gmail.com>
2024-08-14 13:32:54 +0700

    add props

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,9 +1,10 @@
-const Item = ()=> {
-  const name = "พักโรงแรม"   // เพิ่มตัวแปร
-  const amount = 5000
+const Item = (props)=> {        // *รับค่า props(property)
+  // const name = "พักโรงแรม"   // เพิ่มตัวแปร
+  // const amount = 5000
   return (
     <li>
-      {name} <span>-{amount}</span>
+      {/* {name} <span>-{amount}</span> */}
+      {props.title} <span>{props.amount}</span>     {/* แสดงค่า property */}
     </li>
   );
 }
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -2,13 +2,18 @@ import Item from "./Item";
 import "./Transaction.css";
 
 const Transaction = () => {
+  const data = [
+    {title: "ค่ารักษาพยาบาล", amount: 2000},
+    {title: "ค่าน้ำมัน", amount: 5000},
+    {title: "ค่าเช่าบ้าน", amount: 8000},
+    {title: "เงินเดือน", amount: 70000},
+  ]
   return (
     <ul className="item-list">
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
+      <Item title={data[0].title} amount={data[0].amount} />  {/* กำหนดค่า props(property) ที่ส่งไปยัง Item */}
+      <Item title={data[1].title} amount={data[1].amount} /> 
+      <Item title={data[2].title} amount={data[2].amount} />    
+      <Item title={data[3].title} amount={data[3].amount} />    
     </ul>
   );
 };
