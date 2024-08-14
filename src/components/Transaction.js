eae6b386e11741f21c2f524b47c64d4e7588acab
chxmek <Chawanwits.mek@gmail.com>
2024-08-14 14:05:47 +0700

    add loop props

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -3,17 +3,24 @@ import "./Transaction.css";
 
 const Transaction = () => {
   const data = [
-    {title: "ค่ารักษาพยาบาล", amount: 2000},
-    {title: "ค่าน้ำมัน", amount: 5000},
-    {title: "ค่าเช่าบ้าน", amount: 8000},
-    {title: "เงินเดือน", amount: 70000},
-  ]
+    { title: "ค่ารักษาพยาบาล", amount: 2000 },
+    { title: "ค่าน้ำมัน", amount: 5000 },
+    { title: "ค่าเช่าบ้าน", amount: 8000 },
+    { title: "เงินเดือน", amount: 70000 },
+    { title: "จ่ายค่าประกัน", amount: 300 },
+    { title: "ค่าเดินทาง", amount: 800 },
+  ];
   return (
+    // <ul className="item-list">
+    //   <Item title={data[0].title} amount={data[0].amount} />  {/* กำหนดค่า props(property) ที่ส่งไปยัง Item */}
+    //   <Item title={data[1].title} amount={data[1].amount} />
+    //   <Item title={data[2].title} amount={data[2].amount} />
+    //   <Item title={data[3].title} amount={data[3].amount} />
+    // </ul>
     <ul className="item-list">
-      <Item title={data[0].title} amount={data[0].amount} />  {/* กำหนดค่า props(property) ที่ส่งไปยัง Item */}
-      <Item title={data[1].title} amount={data[1].amount} /> 
-      <Item title={data[2].title} amount={data[2].amount} />    
-      <Item title={data[3].title} amount={data[3].amount} />    
+      {data.map((element) => {
+        return <Item title = {element.title} amount = {element.amount}/>;   // loop ค่าแสดงผลแบบ array.map
+      })}
     </ul>
   );
 };
