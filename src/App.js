a0ae61816180b92628fbd3aec479727232ed93d5
chxmek <Chawanwits.mek@gmail.com>
2024-08-15 00:45:14 +0700

    make bottom up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import Transaction from "./components/Transaction";
 import "./App.css"
 import FormComponent from "./components/FormComponent";
+import { v7 as uuidv7 } from 'uuid';
 
 function App() {    // parent component >> App, children component >> Title, Description, Transaction, ...
 
   const design = {color:'red', textAlign:"center", fontSize:"1.5rem"}
+  const initData = [                                // initData >> ข้อมูลที่ถูกนิยามในตอนเริ่มต้น(เป็นข้อมูลตัวอย่าง ยังไม่ได้อ้างอิงตามข้อมูลจริง)
+    {id:1, title: "ค่ารักษาพยาบาล", amount: 2000 },
+    {id:2, title: "ค่าน้ำมัน", amount: 5000 },
+    {id:3, title: "ค่าเช่าบ้าน", amount: 8000 },
+  ];
   
   return (
     <div className="container">
       <h1 style={design}>Statement Application</h1>
       <FormComponent />
-      <Transaction />
+      <Transaction items = {initData}/>             {/* prop ข้อมูลเข้า Transaction */}
     </div>
   );
 }
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,28 +1,14 @@
 import Item from "./Item";
 import "./Transaction.css";
-import { v7 as uuidv7 } from 'uuid';
 
-const Transaction = () => {
-  const data = [
-    {title: "ค่ารักษาพยาบาล", amount: 2000 },
-    {title: "ค่าน้ำมัน", amount: 5000 },
-    {title: "ค่าเช่าบ้าน", amount: 8000 },
-    {title: "เงินเดือน", amount: 70000 },
-    {title: "จ่ายค่าประกัน", amount: 300 },
-    {title: "ค่าเดินทาง", amount: 800 },
-  ];
+const Transaction = (props) => {
+  const {items} = props
   return (
-    // <ul className="item-list">
-    //   <Item title={data[0].title} amount={data[0].amount} />  {/* กำหนดค่า props(property) ที่ส่งไปยัง Item */}
-    //   <Item title={data[1].title} amount={data[1].amount} />
-    //   <Item title={data[2].title} amount={data[2].amount} />
-    //   <Item title={data[3].title} amount={data[3].amount} />
-    // </ul>
 
     <ul className="item-list">
-      {data.map((element) => {
+      {items.map((element) => {
         // return <Item title={element.title} amount={element.amount} key = {uuidv7() />;   // loop ค่าแบบ array.map
-        return <Item {...element} key = {uuidv7()} />;                     // loop ค่าแบบ array.map, spread operator (...element) and key เพื่อให้ app รู้ว่าเป็น Item ตัวไหน
+        return <Item {...element} key = {element.id} />;                     // loop ค่าแบบ array.map, spread operator (...element) and key เพื่อให้ app รู้ว่าเป็น Item ตัวไหน
       })}
     </ul>
   );
